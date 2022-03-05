ddd556859cb0fac15828f37e2e7013ff75b92367
fewtarius <fewtarius@jelos.org>
2022-03-05 13:09:31 -0500

    Set the internal switch, but don't save.

--- es-app/src/guis/GuiMenu.cpp
+++ es-app/src/guis/GuiMenu.cpp
@@ -4032,6 +4032,7 @@ void GuiMenu::openNetworkSettings_batocera(bool selectWifiEnable)
 	internal_wifi->setState(internalmoduleEnabled);
 	s->addWithLabel(_("ENABLE INTERNAL WIFI"), internal_wifi);
 	s->addSaveFunc([internal_wifi] {
+		SystemConf::getInstance()->set("internal.wifi", internalmoduleEnabled ? "1" : "0");
 		if (internal_wifi->getState() == false) {
 			runSystemCommand("/usr/bin/internalwifi disable", "", nullptr);
 		} else {
