99b7b72818f3fd702b0d11d3e530810568405a3a
dhwz <dhwz@gmx.net>
2023-05-30 20:07:19 +0200

    fixes for GCC13

--- es-app/src/Gamelist.h
+++ es-app/src/Gamelist.h
@@ -5,6 +5,7 @@
 #include <unordered_map>
 #include <vector>
 #include <string>
+#include <cstdint>
 
 class SystemData;
 class FileData;
--- es-core/src/utils/ZipFile.h
+++ es-core/src/utils/ZipFile.h
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <cstdint>
 
 namespace Utils
 {
