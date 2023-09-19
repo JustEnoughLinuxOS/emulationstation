4a37b746f267930bc35063fc609801f90759570f
fewtarius <fewtarius@jelos.org>
2023-09-19 13:46:33 +0000

    Calculate the correct brightness when deactivating the screensaver.

--- es-app/src/SystemScreenSaver.cpp
+++ es-app/src/SystemScreenSaver.cpp
@@ -184,7 +184,7 @@ void SystemScreenSaver::stopScreenSaver()
 	if ( screensaver_behavior == "black" )
 	{
 		auto sysbright = SystemConf::getInstance()->get("system.brightness");
-		BrightnessControl::getInstance()->setBrightness(stoi(sysbright));
+		BrightnessControl::getInstance()->setBrightness(stoi(sysbright) * 10);
 	}
 
 	if (mLoadingNext)
